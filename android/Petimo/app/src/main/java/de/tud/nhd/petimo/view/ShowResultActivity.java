086c8055616d87a05c5dc5484b13efad44fa2264
nhd <chuhoangan@gmail.com>
2017-09-03 23:38:04 +0200

    ListFragment for displaying monitor blocks init

---
+++ android/Petimo/app/src/main/java/de/tud/nhd/petimo/view/ShowResultActivity.java
@@ -0,0 +1,16 @@
+package de.tud.nhd.petimo.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import de.tud.nhd.petimo.R;
+
+// TODO implement me
+public class ShowResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_result);
+    }
+}
--- android/Petimo/app/src/main/java/de/tud/nhd/petimo/view/fragments/OffModeFragment.java
+++ android/Petimo/app/src/main/java/de/tud/nhd/petimo/view/fragments/OffModeFragment.java
@@ -5,6 +5,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
